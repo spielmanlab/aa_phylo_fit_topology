48d84c996cd74942aae3c64c8d0d3990f921ca1c
Stephanie J. Spielman <spielman@rowan.edu>
2019-04-25 08:44:59 -0400

    opt q1 as true tree for pandit

--- run_analysis_files/infer_trees.py
+++ run_analysis_files/infer_trees.py
@@ -3,10 +3,6 @@ import re
 import sys
 from copy import deepcopy
 
-true_tree_path    = "simulations/true_trees/"
-hb_path           = "hb_models/"
-
-
 def run_save_iqtree(alignment_file, data_type, model, outname, true_tree, threads):
     
     ### Optimize tree under model ###
@@ -35,43 +31,35 @@ def main():
     repl      = sys.argv[3]
     fitted_tree_path   = sys.argv[4]
     alignment_path = sys.argv[5]
-    pogomodel_path = sys.argv[6]
-    model_file = sys.argv[7]
-    threads   = sys.argv[8]
+    model_file = sys.argv[6]
+    threads   = sys.argv[7]
    
-    
     if tree == "NA":
         rawname = name
         ###### !!!!!!!!! q1 tree here !!!!!!!!!
         true_tree_file = fitted_tree_path + name + "_q1_inferredtree.treefile"
     else:
         rawname        = name + "_" + tree + "_rep" + repl + "_AA"
-        true_tree_file = true_tree_path + tree + ".tree"
+        true_tree_file =  "simulations/true_trees/" + tree + ".tree"
     alignment_file = alignment_path + rawname + ".fasta"
   
     with open(model_file, "r") as f: 
         all_models = f.readlines()
-    print(all_models)   
     use_models = {}
-    # LAC,btree64_bl3.0.tree,5,HIVw,5
-    print (name + "," + tree + "," + repl)
     for line in all_models:
-        print(line)
         if line.startswith(name + "," + tree + "," + repl):
             line2 = line.split(",")
             model = line2[3].strip()
             q = line2[4].strip()
             use_models[q] = model
-    print(use_models)
         
     # quartile models    
     for modelquant in range(1,6):
-        modelquant = str(int_modelquant)
         outname = fitted_tree_path + rawname + "_q" + str(modelquant)
         run_save_iqtree(alignment_file, "AA", use_models[str(modelquant)], outname, true_tree_file, threads)
     ## poisson
     outname = fitted_tree_path + rawname + "_poisson"
-    run_save_iqtree(alignment_file, "AA", "Poisson", outname1, true_tree_file, threads)
+    run_save_iqtree(alignment_file, "AA", "Poisson", outname, true_tree_file, threads)
              
 
 main()
